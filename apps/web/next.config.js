5316d210c13d9a1e352a9eaa30f73b1e97dc2583
Stephanie Zeng <stephanie.zeng@rangle.io>
2024-12-31 15:05:16 -0500

    fix: use dotenv cli to load env

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        UNIFORM_PROJECT_ID: process.env.UNIFORM_PROJECT_ID,
+        UNIFORM_API_KEY: process.env.UNIFORM_API_KEY,
+        UNIFORM_PREVIEW_SECRET: process.env.UNIFORM_PREVIEW_SECRET,
+        UNIFORM_CLI_API_KEY: process.env.UNIFORM_CLI_API_KEY,
+    },
+  };
 
 console.log('Environment in Vercel Build:', process.env.UNIFORM_PREVIEW_SECRET);
 
