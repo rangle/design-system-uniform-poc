d8c3b52c9c27ac1141935552dba9e58b027e39cf
Stephanie Zeng <stephanie.zeng@rangle.io>
2025-01-08 17:13:11 -0500

    feat: adding geo location from vercel header

--- apps/web/uniform/clientContext.ts
+++ apps/web/uniform/clientContext.ts
@@ -5,11 +5,25 @@ import {
   createClientUniformContext,
   useInitUniformContext,
 } from "@uniformdev/canvas-next-rsc/component";
+import { ContextPlugin, enableContextDevTools } from "@uniformdev/context";
+import { useRouter } from "next/navigation";
 
 export const UniformClientContext: ClientContextComponent = ({ manifest }) => {
+  const router = useRouter();
+
   useInitUniformContext(() => {
+    const plugins: ContextPlugin[] = [];
+
+    plugins.push(
+      enableContextDevTools({
+        onAfterMessageReceived: () => {
+          router.refresh();
+        },
+      })
+    );
     return createClientUniformContext({
       manifest,
+      plugins,
       defaultConsent: true,
     });
   });
